059d21db1a6cce0a1b554924e994611d1e6e8d08
0quals2 <wkgek93@gmail.com>
2019-05-27 17:44:38 +0900

    190527

--- 1_python/class.py
+++ 1_python/class.py
@@ -6,3 +6,20 @@ class course:
 
 def greeting():
     return 'Hello!'
+
+
+class Fourth():
+    late = '09:10'
+    finish = '18:00'
+    lunch = '12:10'
+    title= 'Deep learning'
+
+class Student(Fourth):
+    def set_values(self, name, age):
+        self.name = name
+        self.age = age
+
+오창희 = Student('오창희', 99)
+오창희.set_values('창희', 99)
+
+Student(오창희, '창희', 99) #위와 같은 코드 이지만 위의 코드가 더 적합
---
+++ 1_python/dict.py
@@ -0,0 +1,4 @@
+words = {'a': 'apple', 'b': 'banana'}
+
+print(words.get('c', '그런 거 없다'))
+print(word['c'])
